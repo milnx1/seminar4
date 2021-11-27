6e3f5688e7c9c2e808607efb476b1cba40af58fa
milnx1 <milnx1@yandex.ru>
2021-11-27 14:08:20 +0500

    seminar

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+a=int(input("Введите число:"))
+def func(f):
+	if f>-10 and f<10:
+		print(f+5)
+	else:
+		print(f-10)
+
+func(a)
---
+++ exercise2.py
@@ -0,0 +1,6 @@
+a=1
+def func1(b):
+	while b<2.8:
+		b=b+0.2
+		print("%.1f" % b)
+func1(a)
